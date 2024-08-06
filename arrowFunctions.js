7f92ed640cbc0d073ccc564221c15b549a0c2785
Sulaiman Al Refai <sulaimanmarwan54@gmail.com>
2024-08-06 19:54:51 +0300

    update

---
+++ arrowFunctions.js
@@ -0,0 +1,52 @@
+// // Q1)Rewrite the following function as an arrow function:
+// /******************************
+// 	function greet(name) {
+// 	  return `Hello, ${name}!`;
+// 	}
+// ********************************/
+// const greet = (name) => {
+//     return `Hello, ${name}!`;
+// }
+// // console.log(greet("Sulaiman"))
+
+// // Q2) Write a simple arrow function that takes two parameters and returns their sum.
+// const math = (num1, num2) => {
+//     return num1 + num2;
+// }
+// // console.log(math(100, 200))
+
+// const squared = (no1, no2) => {
+//     return no1 ** no2;
+// }
+// console.log(squared(4, 2))
+
+// Q4) [🌶️🌶️] Create an arrow function that takes an array of numbers and returns a new array with each number squared.
+// const numbers=[1,2,3,4]
+// let squared=[]
+
+// numbers.forEach( (num) =>{
+      
+//         squared.push(  num ** 2 );
+    
+// }) 
+// console.log(squared);
+// const numbers=[1, 4, 6, 9, 3, 2, 12, 14, 7]
+// numbers.forEach( (num) => {
+//     console.log(num)
+// })
+
+// const numbers = [100, 2, 3, 5, 9, 6, 12, 9];
+
+// let min=109099999999;
+
+// numbers.forEach( (num)=>{
+
+//     if (min > num){
+//         min = num
+//     }
+// } )
+
+
+
+
+
